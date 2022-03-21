4744551b4b66ff6ccd7338f212ec93969bdfeb43
leopori <100742075+leopori@users.noreply.github.com>
2022-03-20 19:31:03 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+/*Button go top */
+
+window.onscroll = function () {
+    if (document.documentElement.scrollTop > 100) {
+        document.querySelector(".gotopcontainer").classList.add(".show");
+    }
+    else {
+        document.querySelector(".gotopcontainer").classList.remove(".show");
+    }
+}
+
+
+document.querySelector(".gotopcontainer").addEventListener('click', ()=>{
+    window.scrollTo({
+        top:0,
+        behavior: 'smooth'
+    })
+})
+
+
+/*Efecto del fondo de inicio */
+let stars = document.getElementById('stars');
+let moon = document.getElementById('moon');
+let mountains_behind = document.getElementById('mountains_behind');
+let text = document.getElementById('text');
+let btn = document.getElementById('btn');
+let mountains_front = document.getElementById('mountains_front');
+let header = document.querySelector('header');
+
+window.addEventListener('scroll', function () {
+    let value = window.scrollY;
+    stars.style.left = value * 0.25 + 'px';
+    moon.style.top = value * 1.05 + 'px';
+    mountains_behind.style.top = value * 0.75 + 'px';
+    mountains_front.style.top = value * 0 + 'px';
+    text.style.marginRight = value * 4 + 'px';
+    text.style.marginTop = value * 1.5 + 'px';
+    btn.style.marginTop = value * 1.5 + 'px';
+    header.style.top = value * 0.75 + 'px';
+})
+
+
+
